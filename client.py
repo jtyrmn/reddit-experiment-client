4522d109799e6db3d899ba6b51c30dc0d7c88020
jtyrmn <jaketyermanbusiness@gmail.com>
2024-07-20 02:03:25 -0700

    basic API connection

--- client.py
+++ client.py
@@ -1,4 +1,17 @@
 import praw
+from credentials import CREDENTIALS
+
+def run():
+    reddit_client = praw.Reddit(
+        client_id=CREDENTIALS['CLIENT_ID'],
+        client_secret=CREDENTIALS['CLIENT_SECRET'],
+        password=CREDENTIALS['PASSWORD'],
+        user_agent=CREDENTIALS['USERAGENT'],
+        username=CREDENTIALS['USERNAME']
+    )
+
+    for submission in reddit_client.front.new(limit=5):
+        print(f'{submission.title} (by u/{submission.author}): {submission.selftext}')
 
 if __name__ == '__main__':
-    print('hello')
+    run()
